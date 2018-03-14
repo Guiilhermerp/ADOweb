9901e98fcece9deb8cce2e0553f5d733b0ad0ce2
Guilherme ROdrigues <guiigrodrigues@gmail.com>
2018-03-13 22:29:27 -0300

    Servlet e JSP

---
+++ src/main/java/com/mycompany/servlet/AlterarProdutoServlet.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.servlet;
+
+import com.mycompany.adoweb01.Produto;
+import com.mycompany.adoweb01.ProdutoDAO;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author guilherme.gcosta6
+ */
+@WebServlet(name = "AlterarProdutoServlet", urlPatterns = {"/alterar-produto"})
+public class AlterarProdutoServlet extends HttpServlet {
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       HttpSession sessao = request.getSession();
+        if (sessao == null || sessao.getAttribute("usuario") == null) {
+            request.setAttribute("mensagemErro", "Você precisa logar ! ");
+            RequestDispatcher dispatcher
+                    = request.getRequestDispatcher("/index.jsp");
+            dispatcher.forward(request, response);
+
+        }
+        String nome = request.getParameter("nome");
+        String descricao = request.getParameter("descricao");
+        Integer quantidade = Integer.parseInt(request.getParameter("quantidade"));
+        double preco_compra = Double.parseDouble(request.getParameter("preco_compra"));
+        double preco_venda = Double.parseDouble(request.getParameter("preco_venda"));
+        
+
+        Produto produto = new Produto(quantidade, nome, descricao, 0, 0, 0, dt_cadastro);
+
+        produto.setNome(nome);
+        produto.setDescricao(descricao);
+        produto.setQuantidade(0);
+        produto.setPreco_compra(0);
+        produto.setPreco_venda(0);
+
+        try {
+            ProdutoDAO.atualizarProduto(produto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect(request.getContextPath() + "/consultarProduto.jsp");
+
+    }
+}
---
+++ src/main/java/com/mycompany/servlet/CadastroProduto.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.servlet;
+
+import com.mycompany.adoweb01.Produto;
+import com.mycompany.adoweb01.ProdutoDAO;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author guilherme.gcosta6
+ */
+@WebServlet(name = "CadastroProduto", urlPatterns = {"/cadastro-produto"})
+public class CadastroProduto extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String destino;
+
+        HttpSession sessao = request.getSession();
+
+        if (sessao.getAttribute("produto") != null) {
+
+            request.setAttribute("mensagem", "Produto cadastrado ! ");
+            RequestDispatcher dispatcher
+                    = request.getRequestDispatcher("/CadastroProduto.jsp");
+            dispatcher.forward(request, response);
+
+            destino = "CadastrarProduto.jsp";
+
+        } else {
+
+            destino = "CadastroProduto.jsp";
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(destino);
+        dispatcher.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession sessao = request.getSession();
+        if (sessao == null || sessao.getAttribute("usuario") == null) {
+            request.setAttribute("mensagemErro", "Você precisa logar ! ");
+            RequestDispatcher dispatcher
+                    = request.getRequestDispatcher("/index.jsp");
+            dispatcher.forward(request, response);
+
+        }
+
+        String nome = request.getParameter("nome");
+        String descricao = request.getParameter("descricao");
+        int quantidade = -1;
+        double preco_venda = -1;
+        
+        try {
+            quantidade = Integer.parseInt(request.getParameter("estoque"));
+            preco_venda = Double.parseDouble(request.getParameter("preco"));
+        } catch (Exception e) {
+
+        }
+
+        
+
+        produto.setNome(nome);
+        produto.set
+        produto.setEditora(editora);
+        produto.setAno(ano);
+        produto.setQuantidade(quantidade);
+        produto.setPreco(preco);
+        String erro = "";
+        erro = com.aztec.koob.validadores.ValidadorProduto.validarProduto(produto);
+        if (erro.equals("")) {
+            try {
+                ProdutoDAO.inserirProduto(produto);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+             request.setAttribute("mensagem", "Produto cadastrado ! ");
+            RequestDispatcher dispatcher
+                    = request.getRequestDispatcher("/cadastrarProduto.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            request.setAttribute("mensagem", erro);
+            RequestDispatcher dispatcher
+                    = request.getRequestDispatcher("/cadastrarProduto.jsp");
+            dispatcher.forward(request, response);
+        }
+
+    }
+}
